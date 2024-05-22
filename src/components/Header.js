78d1facb0be6d34371ce4fb2f2b3925487db0e68
Hasti Dholakiya <114806574+Hasti-Dholakiya@users.noreply.github.com>
2024-05-22 17:31:44 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import logo from "../assets/investment-calculator-logo.png";
+export default function Header(){
+          return(<header>
+            <img src={logo} alt="Logo here"/>
+            <h1>Investment Calculator</h1>
+          </header>);
+}
